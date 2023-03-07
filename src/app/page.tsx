197f35f4b700a8dd4b85056c2980ce2fd818ae08
Gitter499 <rafayel.amirkhanyan@gmail.com>
2023-03-07 17:48:32 -0500

    (fix) learned some next.js 13...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,37 +3,16 @@ import { Inter } from "next/font/google";
 import "./page.css";
 import { Placeholder } from "@/components/placeholder";
 import { animated } from "@react-spring/web";
-import Head from "next/head";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const metadata = {
-  title: "Ctrl-Shift 2023 Project",
-  description: "A project for 2023 CtrlShift coding competition",
-  twitter: {
-    card: "summary",
-    title: "Ctrl-Shift 2023 Project",
-    description: "A project for 2023 CtrlShift coding competition",
-    creator: "@rafayelamirkha1",
-  },
-};
 
 const PlaceHolder = animated(Placeholder);
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta name="twitter:card" content={metadata.twitter.card} />
-        <meta name="twitter:title" content={metadata.twitter.title} />
-        <meta
-          name="twitter:description"
-          content={metadata.twitter.description}
-        />
-        <meta name="twitter:creator" content={metadata.twitter.creator} />
-      </Head>
       <main>
         <PlaceHolder direction="up" />
         <div className="center">

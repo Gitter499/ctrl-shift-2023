d8b14c339198f046ac537df3ae35030784e5d775
Gitter499 <rafayel.amirkhanyan@gmail.com>
2023-04-08 12:01:54 -0400

    Styled welcome page

--- src/app/(welcome)/welcome/page.tsx
+++ src/app/(welcome)/welcome/page.tsx
@@ -9,13 +9,14 @@ import "./page.css";
 
 import { getUser } from "@/lib/user/getUser";
 import { getSession, useSession } from "next-auth/react";
+import { Space_Mono } from "next/font/google";
 
 type Props = {};
 
-// export const metadata: Metadata = {
-//   title: "Welcome to Yes!",
-//   description: "Please fill out some of the information below to get started.",
-// };
+const spacemono = Space_Mono({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 const WelcomePage = (props: Props) => {
   const [file, setFile] = useState<Blob | undefined>(undefined);
@@ -38,7 +39,6 @@ const WelcomePage = (props: Props) => {
       resume: file,
     });
 
-    
     if (!sendable.resume) {
       alert("Please upload a resume.");
     } else {
@@ -55,49 +55,57 @@ const WelcomePage = (props: Props) => {
 
   return (
     <>
-      <div className="container">
+      <div className={`container ${spacemono.className}`}>
         <div className="welcome-container">
-          <AH2 animationName="slideFromLeft">Welcome to Yes!</AH2>
-          <p>Please fill out some of the information below to get started.</p>
+          <div className="top-text">
+            <AH2 animationName="slideFromLeft">Welcome to Yes!</AH2>
+            <p>Please fill out some of the information below to get started.</p>
+          </div>
           <div className="info-container">
             <form className="info-form" onSubmit={onSubmit}>
-              <div className="bio">
-                <label htmlFor="bio">Bio</label>
-                <textarea
-                  name="bio"
-                  required
-                  id="bio"
-                  cols={30}
-                  rows={10}
-                  placeholder="I love eating pizza #NYC"
-                />
-              </div>
-              <div className="location">
-                <label htmlFor="location">Location</label>
-                <input
-                  required
-                  type="text"
-                  name="location"
-                  id="location"
-                  placeholder="New York, NY"
-                />
-              </div>
-              <div className="website">
-                <label htmlFor="website">Website</label>
-                <input
-                  required
-                  type="text"
-                  name="website"
-                  id="website"
-                  placeholder="https://example.com"
-                />
-              </div>
-              <div className="birthday">
-                <label htmlFor="birthday">Birthday</label>
-                <input type="date" name="birthday" id="birthday" />
+              <div className="raw">
+                <div className="bio">
+                  <label htmlFor="bio">Bio</label>
+                  <textarea
+                    name="bio"
+                    required
+                    id="bio"
+                    cols={30}
+                    rows={10}
+                    spellCheck
+                    
+                    placeholder="I love eating pizza #NYC"
+                  />
+                </div>
+                <div className="location">
+                  <label htmlFor="location">Location</label>
+                  <input
+                    required
+                    type="text"
+                    name="location"
+                    id="location"
+                    placeholder="New York, NY"
+                  />
+                </div>
+                <div className="website">
+                  <label htmlFor="website">Website</label>
+                  <input
+                    required
+                    type="text"
+                    name="website"
+                    id="website"
+                    placeholder="https://example.com"
+                  />
+                </div>
+                <div className="birthday">
+                  <label htmlFor="birthday">Birthday</label>
+                  <input type="date" name="birthday" id="birthday" />
+                </div>
               </div>
               <div className="resume-container">
-                <AH2 animationName="slideFromLeft">Resume</AH2>
+                <AH2 className="resume-text">
+                  Resume
+                </AH2>
                 <p>Upload your resume to get started.</p>
                 <div className="resume-dropzone">
                   <FileUploader
@@ -106,10 +114,16 @@ const WelcomePage = (props: Props) => {
                     types={["PDF"]}
                   />
                 </div>
+                <div className="btn-container">
+                <button
+                  className={`submit-btn ${spacemono.className}`}
+                  type="submit"
+                >
+                  Submit
+                </button>
               </div>
-              <div className="submit-container">
-                <button type="submit">Submit</button>
               </div>
+              
             </form>
           </div>
         </div>
